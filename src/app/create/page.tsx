bb45cf4ccde5cafe79d2eb9ccf45935260e4613d
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-06-13 15:44:49 +0330

    feat: create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { FormEvent } from "react";
+import { Button, TextInput } from "@mantine/core";
+
 import Container from "@techsiro/components/container";
 
 export default function Create() {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event);
+  };
   return (
     <Container>
-      <h1>Create</h1>
+      <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+        <TextInput
+          label="Campaign Name"
+          placeholder="Enter your campaign name"
+          name="campaign_name"
+          id="campaign_name"
+        />
+        <Button variant="filled" className="w-fit" type="submit">
+          Create Campaign
+        </Button>
+      </form>
     </Container>
   );
 }
